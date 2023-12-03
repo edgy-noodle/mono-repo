77d27dd623386393b4a9833370548372dcd1834d
edgy <alexwavresz@gmail.com>
2023-12-03 14:59:04 +0100

    docs: comment on Python choice for writing changes and logic used

--- resources/gha/structure.py
+++ resources/gha/structure.py
@@ -1,18 +1,20 @@
-# Import the fileinput module
+# Saving changes using this script rather than bash (e.g. sed) due to complexity involved in
+# handling | and ` characters while processing multi-line file in the console
+
 import fileinput
 import os
-# Loop through the lines of README.md
+
 with fileinput.input('README.md', inplace=True) as readme:
     replace_struct = False
     for line in readme:
-        # If the line contains the placeholder, print the contents of tree.txt
+        # If we're not in struct, print the contents as they are
         if not replace_struct:
             if '```struct' in line:
                 print(line, end='')
                 replace_struct = True
-            # Otherwise, print the line as it is
             else:
                 print(line, end='')
+        # When struct is found, write the generated structure into the file
         else:
             if '```' in line:
                 with open(os.environ["STRUCTURE"]) as f:
