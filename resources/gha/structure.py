6b28a2735b2b2be122979b6ae66931fafcd2da8a
edgy <alexwavresz@gmail.com>
2023-12-03 13:23:54 +0100

    feat: python script to replace struct

---
+++ resources/gha/structure.py
@@ -0,0 +1,19 @@
+# Import the fileinput module
+import fileinput
+# Loop through the lines of README.md
+with fileinput.input('README.md', inplace=True) as readme:
+    replace_struct = False
+    for line in readme:
+        # If the line contains the placeholder, print the contents of tree.txt
+        if not replace_struct:
+            if '```struct' in line:
+                print(line, end='')
+                replace_struct = True
+            # Otherwise, print the line as it is
+            else:
+                print(line, end='')
+        else:
+            if '```' in line:
+                with open('tree.txt') as f:
+                    print(f.read(), end='```')
+                replace_struct = False
